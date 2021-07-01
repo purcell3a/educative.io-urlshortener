eb98e07df837fc44f6aae172fbb448e236e4c773
Angela Purcell <angelapurcell@Angelas-MBP.attlocal.net>
2021-06-30 21:18:46 -0700

    imported github links

--- main.go
+++ main.go
@@ -3,8 +3,8 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"https://github.com/purcell3a/educative.io-urlshortener/store"
-	"https://github.com/purcell3a/educative.io-urlshortener/key"
+	"github.com/purcell3a/educative.io-urlshortener/store"
+	"github.com/purcell3a/educative.io-urlshortener/key"
 
 )
 
